44cefec3d0309c2eb98859dd221e7b8e34eb599f
DamianoLandi <damianolandi01@gmail.com>
2021-07-26 17:46:30 +0200

    Switch change

--- js/script.js
+++ js/script.js
@@ -6,25 +6,9 @@ const cardSubtitle = document.getElementsByClassName("card-subtitle");
 
 for(let i = 0; i < icons.length; i++){
     cardTitle[i].innerHTML = `<i class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"></i>`
-    switch(icons[i].type){
-        case "animal":
-            cardTitle[i].classList.add("animal");
-            break;
-        case "vegetable":
-            cardTitle[i].classList.add("vegetable");
-            break;
-        case "user":
-            cardTitle[i].classList.add("user");
-            break;
-        case "company":
-            cardTitle[i].classList.add("company");
-            break;
-    }
+    cardTitle[i].classList.add(icons[i].type);
 };
 
 for(let i = 0; i < icons.length; i++){
     cardSubtitle[i].innerHTML = `${icons[i].name}`
 };
-
-
-
